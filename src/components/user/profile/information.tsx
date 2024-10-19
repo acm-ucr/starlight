3526ce0b9ace49d301c526b41c8a08d455eb26da
Andrew Khadder <akhad007@ucr.edu>
2024-10-19 15:57:34 -0700

    format

--- src/components/user/profile/information.tsx
+++ src/components/user/profile/information.tsx
@@ -3,18 +3,22 @@ import Input from "../../global/inputs/input";
 import Radio from "../../global/inputs/radio";
 import Text from "../../global/inputs/text";
 import Textarea from "@/components/global/inputs/textarea";
-import { nameOptions, majorOptions, graduationOptions, questionOptions, techStackOptions } from "@/data/profile/profile-information";
+import {
+  nameOptions,
+  majorOptions,
+  graduationOptions,
+  questionOptions,
+  techStackOptions,
+} from "@/data/profile/profile-information";
 
 const Information = () => {
   return (
     <div className="h-1/2 rounded-xl border-4 border-gray-300">
-      <Text 
-        meta={nameOptions}
-      />
+      <Text meta={nameOptions} />
       <Select meta={majorOptions} />
       <Input />
-      <Radio meta={graduationOptions}/>
-      <Textarea meta={questionOptions}/>
+      <Radio meta={graduationOptions} />
+      <Textarea meta={questionOptions} />
       <Select meta={techStackOptions} />
     </div>
   );
