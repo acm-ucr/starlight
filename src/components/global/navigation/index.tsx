8404558a0a3e3bfa528ef18b886088d7c907f991
Ricardo Galeano <ricardogaleano@Ricardos-MacBook-Air.local>
2024-10-22 16:07:19 -0700

    Created top NavBar using shadcn

--- src/components/global/navigation/index.tsx
+++ src/components/global/navigation/index.tsx
@@ -1,11 +1,65 @@
+"use client";
+
+import * as React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+import logo from "@/public/Assets/ACMStarlightLogo.webp";
 
 const Navigation = () => {
+  const pathname = usePathname();
   return (
-    <div className="flex gap-4 bg-starlight-blue text-white">
-      <Link href="/user/applications">Applications</Link>
-      <Link href="/user/profile">Profile</Link>
-    </div>
+    <NavigationMenu className="flex max-w-full items-center justify-between bg-starlight-blue p-4 text-white">
+      <Link className="flex items-center" href="/">
+        <div className="relative h-14 w-14">
+          <Image
+            className="object-contain"
+            src={logo}
+            alt="ACM Starlight Logo"
+          />
+        </div>
+      </Link>
+
+      <NavigationMenuList className="mx-14 flex gap-8">
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link
+              href="/user/applications"
+              className={`text-white ${
+                pathname === "/user/applications"
+                  ? "underline underline-offset-8"
+                  : ""
+              }`}
+            >
+              Applications
+            </Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link
+              href="/user/profile"
+              className={`text-white ${
+                pathname === "/user/profile"
+                  ? "underline underline-offset-8"
+                  : ""
+              }`}
+            >
+              Profile
+            </Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
 
