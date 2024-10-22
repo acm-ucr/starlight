c7967660b75a03dcd3d653cddfef2c599064e708
Rishab Pillai <rishabhpillai.me@gmail.com>
2024-10-21 19:19:11 -0700

    modified button further

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 interface ButtonProps {
   text: string;
-  onClick: (params: string) => void;
+  onClick: () => void;
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       className="rounded-md bg-blue-900 px-8 py-2 font-serif text-[vw] text-white"
-      onClick={() => onClick(text)}
+      onClick={onClick}
     >
       {text}
     </button>
