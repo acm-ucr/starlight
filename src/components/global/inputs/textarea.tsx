9d1c2bdec36471628428d3568bc2286c606aa5b0
Charleen Chen <charleenc485@gmail.com>
2024-10-21 19:00:48 -0700

    Created Textarea from shadcn

--- src/components/global/inputs/textarea.tsx
+++ src/components/global/inputs/textarea.tsx
@@ -1,15 +1,15 @@
 import { TextareaInput } from "@/types/questions";
-import { Label } from "@/components/ui/label"
-import { Textarea as TextareShadCN} from "@/components/ui/textarea"
+import { Label } from "@/components/ui/label";
+import { Textarea as TextareShadCN } from "@/components/ui/textarea";
 
 const Textarea = ({ meta }: { meta: TextareaInput }) => {
   const { title, placeholder } = meta;
   return (
     <div className="grid w-full gap-1.5">
-        <Label htmlFor="message">{title}</Label>
-      <TextareShadCN placeholder= {placeholder} id="message" />
+      <Label htmlFor="message">{title}</Label>
+      <TextareShadCN placeholder={placeholder} id="message" />
     </div>
-  )
-}
+  );
+};
 
 export default Textarea;
