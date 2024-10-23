edb91d0008a147b669aa61929747d100735c98c7
Andrew Khadder <akhad007@ucr.edu>
2024-10-22 17:22:31 -0700

    fix build error

--- src/data/profile/profile-information.ts
+++ src/data/profile/profile-information.ts
@@ -1,30 +1,42 @@
-export const nameOptions = {
-  type: "text",
-  title: "Name",
-  placeholder: "Enter a Name",
-  maxLength: 250,
-};
+import { Questions } from "@/types/questions";
 
-export const majorOptions = {
-  title: "Major",
-  options: ["Computer Science", "Computer Engineering", "Data Science"],
-  value: "",
-  disabled: false,
-};
-
-export const graduationOptions = {
-  title: "Graduation Year",
-  options: ["2025", "2026", "2027", "2028"],
-};
-
-export const questionOptions = {
-  title: "Why do you want to join ACM Ignite?",
-  placeholder: "I want to join ACM Ignite because...",
-};
-
-export const techStackOptions = {
-  title: "Tech Stack",
-  options: ["Typescript", "Javascript", "C/C++"],
-  value: "",
-  disabled: false,
-};
+export const questions: Questions[] = [
+  {
+    type: "text",
+    title: "Name",
+    placeholder: "Enter a Name",
+    maxLength: 250,
+    disabled: false,
+    value: "",
+  },
+  {
+    type: "select",
+    title: "Major",
+    options: ["Computer Science", "Computer Engineering", "Data Science"],
+    value: "",
+    disabled: false,
+  },
+  {
+    title: "Graduation Year",
+    options: [2025, 2026, 2027, 2028],
+    type: "radio",
+    disabled: false,
+    value: "",
+  },
+  // eventually should be an input thing here, its not in yet though
+  {
+    title: "Why do you want to join ACM Ignite?",
+    placeholder: "I want to join ACM Ignite because...",
+    type: "textarea",
+    maxLength: 250,
+    disabled: false,
+    value: "",
+  },
+  {
+    type: "select",
+    title: "Tech Stack",
+    options: ["Typescript", "Javascript", "C/C++"],
+    value: "",
+    disabled: false,
+  },
+];
