8ebafcde9fbd89b25eb617c749e97e598f1a899e
stanleylew5 <stanley43595@gmail.com>
2025-06-17 23:02:29 -0700

    what is going on

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/utils/auth";
 
+export const dynamic = "force-dynamic";
 const handler = NextAuth(authOptions());
 export { handler as GET, handler as POST };
