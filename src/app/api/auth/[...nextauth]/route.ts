c1883af98a4edc3e815b28afe5c7f732821bb6a0
stanleylew5 <stanley43595@gmail.com>
2025-06-17 23:17:07 -0700

    man

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,8 @@ import NextAuth from "next-auth";
 import { getAuthOptions } from "@/utils/auth";
 
 export const dynamic = "force-dynamic";
-const handler = NextAuth(getAuthOptions());
+const handler = async (req: Request, res: Response) => {
+  return NextAuth(getAuthOptions())(req, res);
+};
+
 export { handler as GET, handler as POST };
