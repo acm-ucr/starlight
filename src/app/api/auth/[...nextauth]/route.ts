02f03bea3d723311f5469e23fe497829f3ba23b9
stanleylew5 <stanley43595@gmail.com>
2025-06-17 23:25:15 -0700

    why does it break

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,9 @@
 import NextAuth from "next-auth";
 import { getAuthOptions } from "@/utils/auth";
-import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export const GET = (req: NextRequest) => {
-  return NextAuth(getAuthOptions())(req);
-};
+const handler = NextAuth(getAuthOptions());
 
-export const POST = (req: NextRequest) => {
-  return NextAuth(getAuthOptions())(req);
-};
+export const GET = handler;
+export const POST = handler;
