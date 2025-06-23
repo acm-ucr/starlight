3135895d4a26231c8f8b4479450d1c7662d6c0f3
qhgill <qhgill2@gmail.com>
2025-06-22 22:30:22 -0700

    questions migration wip

--- src/components/form/form/questions.tsx
+++ src/components/form/form/questions.tsx
@@ -1,11 +1,44 @@
 import Checkbox from "@/components/checkbox";
+import { Dispatch, SetStateAction } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  Description,
+  TextInput,
+  CheckboxInput,
+  RadioInput,
+  TextareaInput,
+  SelectInput,
+  ToggleInput,
+  SliderInput,
+} from "@/types/forms";
+type TField =
+  | Description
+  | TextInput
+  | CheckboxInput
+  | RadioInput
+  | TextareaInput
+  | SelectInput
+  | ToggleInput
+  | SliderInput;
 
-const Questions = ({
+interface QuestionProps<TObj> {
+  fields: Record<string,TField>;
+  object: TObj;
+  setObject: Dispatch<SetStateAction<TObj>>;
+  onSubmit: (
+    setLoading: (value: boolean) => void,
+    setState: (value: number) => void,
+  ) => Promise<void> | void;
+  loading: boolean;
+  setLoading: (value: boolean) => void;
+  setState: Dispatch<SetStateAction<number>>;
+}
+
+const Questions = <TObj,>({
   fields,
   object,
   setObject,
@@ -13,8 +46,8 @@ const Questions = ({
   loading,
   setLoading,
   setState,
-}) => {
-  const handleClick = (option, field) => {
+}: QuestionProps<TObj>) => {
+  const handleClick = (option: string, field: string) => {
     setObject({
       ...object,
       [field]: option,
@@ -41,20 +74,20 @@ const Questions = ({
           {field.input === "input" && (
             <>
               <div className="pb-1">
-                <Label htmlFor={field.name} className="font-semibold">
-                  {field.title}
-                  {field.required && <span className="text-red-500">*</span>}
+                <Label htmlFor={(field as TextInput).name} className="font-semibold">
+                  {(field as TextInput).title}
+                  {(field as TextInput).required && <span className="text-red-500">*</span>}
                 </Label>
               </div>
               <Input
-                id={field.name}
-                type={field.type}
-                placeholder={field.placeholder}
-                value={object[field.name]}
-                maxLength={field.maxLength}
-                disabled={!field.editable}
+                id={(field as TextInput).name}
+                type={(field as TextInput).type}
+                placeholder={(field as TextInput).placeholder}
+                value={object[(field as TextInput).name]}
+                maxLength={(field as TextInput).maxLength}
+                disabled={!(field as TextInput).editable}
                 onChange={(e) =>
-                  setObject({ ...object, [field.name]: e.target.value })
+                  setObject({ ...object, [(field as TextInput).name]: e.target.value })
                 }
               />
             </>
@@ -62,22 +95,22 @@ const Questions = ({
           {field.input === "checkboxes" && (
             <>
               <p className="mb-1 font-semibold">
-                {field.text}
-                {field.required && <span className="text-red-500">*</span>}
+                {(field as CheckboxInput).text}
+                {(field as CheckboxInput).required && <span className="text-red-500">*</span>}
               </p>
               <div className="grid gap-2 md:grid-cols-2">
-                {field.options.map((option, i) => (
+                {(field as CheckboxInput).options.map((option, i) => (
                   <Checkbox
                     id={option.id}
-                    checked={object[field.field].includes(option)}
+                    checked={object[(field as CheckboxInput).field].includes(option)}
                     onClick={() => {
                       setObject({
                         ...object,
-                        [field.field]: object[field.field].includes(option)
-                          ? object[field.field].filter(
+                        [(field as CheckboxInput).field]: object[(field as CheckboxInput).field].includes(option)
+                          ? object[(field as CheckboxInput).field].filter(
                               (item) => item !== option,
                             )
-                          : [...object[field.field], option],
+                          : [...object[(field as CheckboxInput).field], option],
                       });
                     }}
                     key={i}
@@ -91,15 +124,15 @@ const Questions = ({
           {field.input === "radio" && (
             <>
               <p className="mb-1 font-semibold">
-                {field.text}
-                {field.required && <span className="text-red-500">*</span>}
+                {(field as RadioInput).text}
+                {(field as RadioInput).required && <span className="text-red-500">*</span>}
               </p>
               <RadioGroup
-                value={object[field.field]}
+                value={object[(field as RadioInput).field]}
                 onValueChange={(value) => handleClick(value, field.field)}
               >
                 <div className="grid grid-cols-2">
-                  {Object.values(field.options).map((option, index) => (
+                  {Object.values((field as RadioInput).options).map((option, index) => (
                     <div
                       className="mb-2 flex items-center space-x-2"
                       key={index}
@@ -115,21 +148,21 @@ const Questions = ({
           {field.input === "textarea" && (
             <>
               <div className="mb-1">
-                <Label htmlFor={field.name} className="font-semibold">
-                  {field.title}
-                  {field.required && <span className="text-red-500">*</span>}
+                <Label htmlFor={(field as TextareaInput).name} className="font-semibold">
+                  {(field as TextareaInput).title}
+                  {(field as TextareaInput).required && <span className="text-red-500">*</span>}
                 </Label>
               </div>
               <Textarea
                 className="w-full resize-none border-1 border-black pl-3 placeholder:text-gray-200 focus:outline-none"
                 maxLength={500}
-                value={object[field.name]}
+                value={object[(field as TextareaInput).name]}
                 onChange={(e) =>
-                  setObject({ ...object, [field.name]: e.target.value })
+                  setObject({ ...object, [(field as TextareaInput).name]: e.target.value })
                 }
-                placeholder={field.placeholder}
-                rows={field.rows}
-                title={field.title}
+                placeholder={(field as TextareaInput).placeholder}
+                rows={(field as TextareaInput).rows}
+                title={(field as TextareaInput).title}
               />
             </>
           )}
