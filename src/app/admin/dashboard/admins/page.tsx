1c70a05974c81a79d9ef4813025266f9c2939ebd
stanleylew5 <stanley43595@gmail.com>
2025-06-24 23:41:56 -0700

    does this solve problems

--- src/app/admin/dashboard/admins/page.tsx
+++ src/app/admin/dashboard/admins/page.tsx
@@ -1,15 +1,27 @@
-import { SearchParams } from "@/types/dashboard";
 import Admins from "@/components/admin/dashboards/admins";
+import { SearchParams } from "@/types/dashboard";
+
 export const metadata = {
   title: "Admin | Admins",
 };
 
-interface searchParamsProps {
-  searchParams: SearchParams;
+interface PageProps {
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-const Page = ({ searchParams }: searchParamsProps) => {
-  return <Admins searchParams={searchParams} />;
+const Page = ({ searchParams }: PageProps) => {
+  const parsedSearchParams: SearchParams = {
+    index: Number(searchParams.index) || 0,
+    size: Number(searchParams.size) || 10,
+    first: typeof searchParams.first === "string" ? searchParams.first : "",
+    last: typeof searchParams.last === "string" ? searchParams.last : "",
+    direction:
+      searchParams.direction === "prev" || searchParams.direction === "next"
+        ? searchParams.direction
+        : "next",
+  };
+
+  return <Admins searchParams={parsedSearchParams} />;
 };
 
 export default Page;
