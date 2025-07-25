4c5eb645268e1e62a0ced31e722bfc7772a81ba4
unknown <twu148@ucr.edu>
2025-07-25 00:58:29 -0700

    feat: Remake landing component, add League Spartan font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import About from "@/components/home/about";
 import Footer from "@/components/home/footer";
 import Landing from "@/components/home/landing";
 import TrackCards from "@/components/home/trackcards";
 const Home = () => {
   return (
-    <div className="bg-starlight-gray-primary">
+    <div className="to-starlight-blue-primary bg-starlight-blue-primary bg-gradient-to-b from-black/35">
       <Landing />
-      <About />
       <TrackCards />
       <Footer />
     </div>
